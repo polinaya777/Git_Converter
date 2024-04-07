c0d898934327ca21463b707b490b2912387ce049
Polina.Ya <polinaya777@gmail.com>
2024-04-07 17:56:52 +0200

    Add temperature.py module

---
+++ temperature.py
@@ -0,0 +1,11 @@
+# Module temperature
+# Description: This module contains the Temperature class which is used to
+# convert temperatures between Celsius and Fahrenheit
+
+# function to convert celsius to fahrenheit
+def celsius_to_fahrenheit(celsius):
+    return celsius * 9.0 / 5 + 32
+
+# function to convert fahrenheit to celsius
+def fahrenheit_to_celsius(fahrenheit):
+    return (fahrenheit - 32) * 5.0 / 9
